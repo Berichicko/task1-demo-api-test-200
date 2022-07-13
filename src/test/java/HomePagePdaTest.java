104e117f7d514eaa39b434061ebc1c24a34c3338
Aliaksandr Zasinets <aliaksandr.zasinets@gmail.com>
2022-07-13 17:46:35 +0300

    feat: add class

--- src/test/java/HomePagePdaTest.java
+++ src/test/java/HomePagePdaTest.java
@@ -1,17 +1,39 @@
+import org.apache.commons.io.IOUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
-import java.io.IOException;
+
 
 
 public class HomePagePdaTest {
 
-
     @Test
-    public void restApiPda () throws IOException {
-        CloseableHttpClient client = HttpClients.createDefault();
-        CloseableHttpResponse response = client.execute(new HttpGet("https://4pda.to/"));
+    public void testApiPda() {
+        try {
+            CloseableHttpClient client = HttpClients.createDefault();
+            CloseableHttpResponse response = client.execute(new HttpGet("https://4pda.to/"));
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                String data = IOUtils.toString(entity.getContent(), "cp1251");
+
+                System.out.println("Data" + data);
+            }
+            System.out.println();
+
+            for (Header header : response.getAllHeaders()) {
+                System.out.println(header.getName() + ":" + header.getValue());
+            }
+            System.out.println();
+            System.out.println("Protocol version" + response.getProtocolVersion());
+            System.out.println("Status code" + response.getStatusLine().getStatusCode());
+            System.out.println("Reason phrase" + response.getStatusLine().getReasonPhrase());
+            System.out.println("Status line" + response.getStatusLine().toString());
+        } catch (Throwable cause) {
+            cause.printStackTrace();
+        }
     }
 }
